be8f538f1e5a17ceb91c798574798bb3a080752e
Amir Ingher <amir.ingher26@gmail.com>
2024-11-12 01:03:30 +0200

    local git fix

---
+++ config.py
@@ -0,0 +1,13 @@
+models = [
+    {
+        "name": "OLLAMA",
+        "model": "llama3:8b-instruct-q4_0",
+        "config": {"temperature": 0.7}
+    },
+    {
+        "name": "OpenAI",
+        "model": "gpt-3.5-turbo",
+        "api_key": "" #Your OpenAI API key here
+        "config": {"temperature": 0.7}
+    }
+]
